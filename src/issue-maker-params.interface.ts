de71f76f5952eb9b5481ee9a68dec24c3cd65be0
Vajahath Ahmed <vajuoff.1@gmail.com>
2019-02-21 17:28:50 +0530

    issuemaker interface changes

--- src/issue-maker-params.interface.ts
+++ src/issue-maker-params.interface.ts
@@ -4,4 +4,9 @@ export interface IIssueMakerParams {
   projectId: string | number;
   endPoint: string;
   privateToken: string;
+  // db details
+  labels?: 'string'; // label already created
+  resLocals?: any; // for showing in issue description.
+  databaseHost?: string; // for showing in issue description.
+  databaseName?: string; // for showing in issue description.
 }
